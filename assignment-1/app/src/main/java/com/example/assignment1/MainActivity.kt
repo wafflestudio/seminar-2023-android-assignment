e83838edf0ea9d5d00e6c13687ba50416b67dfa2
mscwrd02 <ehdtodvodl@gmail.com>
2023-09-13 00:08:17 +0900

    make calculator

--- assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
+++ assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.assignment1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 
@@ -11,15 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // val editTextId = findViewById<EditText>(R.id.editTextId)
-
-//    val textId = editTextId.text.toString()
-//    val editTextPw = findViewById<EditText>(R.id.editTextPw)
-//    val textPw = editTextPw.text.toString()
-
+        val editTextId = findViewById<EditText>(R.id.editTextId)
+        val editTextPw = findViewById<EditText>(R.id.editTextPw)
         val logInButton = findViewById<Button>(R.id.Button)
         logInButton.setOnClickListener{
+            val textId = editTextId.text.toString()
+            val textPw = editTextPw.text.toString()
+
             val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("Id", textId)
+            intent.putExtra("Pw", textPw)
+
             startActivity(intent)
         }
     }
--- assignment-1/app/src/main/java/com/example/assignment1/SecondActivity.kt
+++ assignment-1/app/src/main/java/com/example/assignment1/SecondActivity.kt
@@ -1,12 +1,47 @@
 package com.example.assignment1
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 
 class SecondActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val userIdTextView = findViewById<TextView>(R.id.UserId)
+        userIdTextView.text = intent.getStringExtra("Id")
+        val userPwTextView = findViewById<TextView>(R.id.UserPw)
+        userPwTextView.text = intent.getStringExtra("Pw")
+
+        val plusOperator = findViewById<TextView>(R.id.plus)
+        val minusOperator = findViewById<TextView>(R.id.minus)
+        val multiplyOperator = findViewById<TextView>(R.id.multiply)
+        val divideOperator = findViewById<TextView>(R.id.divide)
+        val operator = findViewById<TextView>(R.id.operator)
+        val result = findViewById<TextView>(R.id.result)
+        val firstInput = findViewById<EditText>(R.id.firstInput)
+        val secondInput = findViewById<EditText>(R.id.secondInput)
+        plusOperator.setOnClickListener {
+            operator.text = "+"
+            result.text = (firstInput.text.toString().toInt() + secondInput.text.toString().toInt()).toString()
+        }
+        minusOperator.setOnClickListener {
+            operator.text = "-"
+            result.text = (firstInput.text.toString().toInt() - secondInput.text.toString().toInt()).toString()
+        }
+        multiplyOperator.setOnClickListener {
+            operator.text = "*"
+            result.text = (firstInput.text.toString().toInt() * secondInput.text.toString().toInt()).toString()
+        }
+        divideOperator.setOnClickListener {
+            operator.text = "/"
+            result.text = (firstInput.text.toString().toInt() / secondInput.text.toString().toInt()).toString()
+        }
+
     }
 }
