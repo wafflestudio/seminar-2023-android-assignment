260a14eb6e5ad5ee5825bd008b1963d0f17657b6
mscwrd02 <ehdtodvodl@gmail.com>
2023-09-12 21:39:20 +0900

    page 1 complete

--- assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
+++ assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.assignment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // val editTextId = findViewById<EditText>(R.id.editTextId)
+
+//    val textId = editTextId.text.toString()
+//    val editTextPw = findViewById<EditText>(R.id.editTextPw)
+//    val textPw = editTextPw.text.toString()
     }
+
 }
