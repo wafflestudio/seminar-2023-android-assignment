d683c095da6494175ffb913c9fbfe9b876b54083
mscwrd02 <ehdtodvodl@gmail.com>
2023-09-13 00:20:23 +0900

    make Toast(I cann't move toast to center of display...)

--- assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
+++ assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -3,9 +3,10 @@ package com.example.assignment1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.Gravity
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,12 +19,18 @@ class MainActivity : AppCompatActivity() {
         logInButton.setOnClickListener{
             val textId = editTextId.text.toString()
             val textPw = editTextPw.text.toString()
+            if(textPw.length <5){
+                val toast = Toast.makeText(this, "유효하지 않은 비밀번호입니다", Toast.LENGTH_SHORT)
+                toast.setGravity(Gravity.CENTER, 0, 0)
+                toast.show()
+            }
+            else {
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("Id", textId)
+                intent.putExtra("Pw", textPw)
 
-            val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra("Id", textId)
-            intent.putExtra("Pw", textPw)
-
-            startActivity(intent)
+                startActivity(intent)
+            }
         }
     }
 
