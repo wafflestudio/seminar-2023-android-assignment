a51cb0dbc4cce72f4cbbbb1ff58c7760303d95b5
2019-13759 <kevin9922@snu.ac.kr>
2023-09-19 01:31:23 +0900

    Page 1 + button to Page 2

--- assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
+++ assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.assignment1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val login = findViewById<TextView>(R.id.login)
+
+        login.setOnClickListener{
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
