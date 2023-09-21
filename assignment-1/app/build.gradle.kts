f8fd6686bbcb1bde914353eba776097c79f186e6
kkomul1 <kkomul1@snu.ac.kr>
2023-09-21 23:41:14 +0900

    assignment1

--- assignment-1/app/build.gradle.kts
+++ assignment-1/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.assignment1"
+    namespace = "com.example.myfirstapp"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.assignment1"
+        applicationId = "com.example.myfirstapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -20,10 +20,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
