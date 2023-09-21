c70ee51308b19bb3d6035f535a9bc01538517b26
Sieun Park <susanpark0@snu.ac.kr>
2023-09-21 22:54:55 +0900

    Add files via upload

--- assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
+++ assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.assignment1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //계산기
+    }
+}
+
+class SecondActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second) //로그인
+        val textView =  findViewById<TextView>(R.id.loginTitle)
+
+        textView.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
