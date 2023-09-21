5ae79bebd07e30e54cb5ca88c581925a45c1ada8
kevin990222 <kevin9922@snu.ac.kr>
2023-09-21 13:46:37 +0900

    complete

--- assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
+++ assignment-1/app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.assignment1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +13,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val login = findViewById<TextView>(R.id.login)
+        val Id = findViewById<EditText>(R.id.Id)
+        val password = findViewById<EditText>(R.id.password)
 
         login.setOnClickListener{
-            val intent = Intent(this,SecondActivity::class.java)
-            startActivity(intent)
+           val pass = password.text.toString()
+
+            if(pass.length<5){
+                Toast.makeText(this,"유효하지 않은 비밀번호입니다.",Toast.LENGTH_SHORT).show()
+            }
+
+            else{
+                val intent = Intent(this,SecondActivity::class.java)
+                intent.putExtra("Id",Id.text.toString())
+                intent.putExtra("password",password.text.toString())
+                startActivity(intent)
+            }
         }
     }
 }
--- assignment-1/app/src/main/java/com/example/assignment1/SecondActivity.kt
+++ assignment-1/app/src/main/java/com/example/assignment1/SecondActivity.kt
@@ -2,10 +2,59 @@ package com.example.assignment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+
 class SecondActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_second)
+
+        val Id = intent.getStringExtra("Id")
+        val password = intent.getStringExtra("password")
+
+        val displayId = findViewById<TextView>(R.id.idView)
+        val displayPassword = findViewById<TextView>(R.id.passwordView)
+
+        displayId.text = "$Id"
+        displayPassword.text = "$password"
+
+        val plus = findViewById<TextView>(R.id.plus)
+        val minus = findViewById<TextView>(R.id.minus)
+        val multiply = findViewById<TextView>(R.id.multiply)
+        val divide = findViewById<TextView>(R.id.divide)
+
+        val firstNum = findViewById<EditText>(R.id.firstNum)
+        val secondNum = findViewById<EditText>(R.id.secondNum)
+        val operator = findViewById<TextView>(R.id.operator)
+        val result = findViewById<TextView>(R.id.result)
+
+        plus.setOnClickListener{
+            val num1 = firstNum.text.toString().toIntOrNull() ?: 0
+            val num2 = secondNum.text.toString().toIntOrNull() ?: 0
+            operator.text = "+"
+            result.text = (num1 + num2).toString()
+        }
+
+        minus.setOnClickListener {
+            val num1 = firstNum.text.toString().toIntOrNull() ?: 0
+            val num2 = secondNum.text.toString().toIntOrNull() ?: 0
+            operator.text = "-"
+            result.text = (num1 - num2).toString()
+        }
+
+        multiply.setOnClickListener {
+            val num1 = firstNum.text.toString().toIntOrNull() ?: 0
+            val num2 = secondNum.text.toString().toIntOrNull() ?: 0
+            operator.text = "*"
+            result.text = (num1 * num2).toString()
+        }
+
+        divide.setOnClickListener {
+            val num1 = firstNum.text.toString().toIntOrNull() ?: 0
+            val num2 = secondNum.text.toString().toIntOrNull() ?: 0
+            operator.text = "/"
+            result.text = (num1 / num2).toString()
+        }
     }
 }
