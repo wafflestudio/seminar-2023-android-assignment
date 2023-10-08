c717b951c3e90ceca6e470472af3af3a299bd44b
plgafhd <141345525+plgafhd@users.noreply.github.com>
2023-10-08 15:02:06 +0900

    Async Programming 8 까지

--- assignment-3/app/build.gradle.kts
+++ assignment-3/app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("com.squareup.moshi:moshi:1.14.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
     implementation("com.google.dagger:hilt-android:2.47")
-    implementation("com.google.dagger:hilt-android-compiler:2.47")
+    kapt("com.google.dagger:hilt-android-compiler:2.47")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
