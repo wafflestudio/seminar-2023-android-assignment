c574003c77933924fc27229d53cba36080fc1c63
junahjeon2002 <junahjeon2002@snu.ac.kr>
2023-11-17 23:32:05 +0900

    수정2

--- assignment-3/app/build.gradle.kts
+++ assignment-3/app/build.gradle.kts
@@ -1,31 +1,22 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("dagger.hilt.android.plugin")
     id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-parcelize")
 }
 
-
-
 android {
-    namespace ="com.assignment3"
+    namespace = "com.jutak.assignment3"
     compileSdk = 34
-
-
-    viewBinding{
-        enable = true
-    }
-
     defaultConfig {
         applicationId = "com.jutak.assignment3"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
-
+        versionCode = 1
+        versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -41,46 +32,28 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
-        languageVersion = "1.5"
     }
-
-    kapt{
-        generateStubs = true
+    buildFeatures {
+        viewBinding = true
     }
-
-
-
 }
-
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
-    //implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    //implementation ("androidx.activity:activity-ktx:1.3.1")
-    //implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
-    implementation ("com.google.dagger:hilt-android:2.42")
-   // implementation ("com.squareup.moshi:moshi-kotlin:1.12.0")
-    kapt ("com.squareup.moshi:moshi-kotlin-codegen:1.12.0")
-    implementation("com.squareup.moshi:moshi:1.14.0")
-    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
-    //implementation ("com.google.dagger:hilt-android:2.x.x")
-    //kapt ("com.google.dagger:hilt-compiler:2.x.x")
-
-    //implementation("com.google.dagger:hilt-android:2.42")
-    kapt("com.google.dagger:hilt-compiler:2.42")
-    implementation ("com.google.android.material:material:1.6.1")
-    implementation ("androidx.activity:activity-ktx:1.3.1")
-
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
-    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.moshi:moshi:1.14.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation("com.google.dagger:hilt-android:2.47")
+    kapt("com.google.dagger:hilt-android-compiler:2.47")
+    annotationProcessor("com.google.dagger:hilt-android-compiler:2.47")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
 }
--- assignment-3/build.gradle.kts
+++ assignment-3/build.gradle.kts
@@ -1,17 +1,11 @@
-
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    kotlin("android") version "1.9.0" apply false
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
+    buildscript {
+        dependencies {
+            classpath("com.google.dagger:hilt-android-gradle-plugin:2.47")
+        }
     }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0-beta01")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.0")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
-
-    }
-}
