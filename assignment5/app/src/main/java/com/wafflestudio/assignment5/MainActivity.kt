9fe0d39a405e7f3f369a127cc3ac5f5ebdfee1d2
양주현 <didwngus01@naver.com>
2023-11-26 17:43:51 +0900

    remove theme

--- assignment5/app/src/main/java/com/wafflestudio/assignment5/MainActivity.kt
+++ assignment5/app/src/main/java/com/wafflestudio/assignment5/MainActivity.kt
@@ -3,44 +3,12 @@ package com.wafflestudio.assignment5
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.wafflestudio.assignment5.ui.theme.Assignment5Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Assignment5Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            // TODO
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Assignment5Theme {
-        Greeting("Android")
     }
 }
