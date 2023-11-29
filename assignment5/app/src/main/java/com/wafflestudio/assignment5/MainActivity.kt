d2a1a0628ea9086b925280ed8228826458359607
plgafhd <plgafhd@snu.ac.kr>
2023-11-28 16:29:09 -0800

    튜토리얼, 시계 구현

--- assignment5/app/src/main/java/com/wafflestudio/assignment5/MainActivity.kt
+++ assignment5/app/src/main/java/com/wafflestudio/assignment5/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.wafflestudio.assignment5
 
-import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,25 +22,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.East
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material.icons.filled.West
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -48,31 +48,21 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
-import androidx.navigation.NavController
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*
-        setContent {
-            MaterialTheme {
-                Surface(modifier = Modifier.fillMaxSize(),color = MaterialTheme.colorScheme.background) {
-                    MessageCard(Message("Android","JC"))
-                }
-            }
-        }
-        */
         setContent {
             MaterialTheme {
                 Surface {
@@ -85,8 +75,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-data class Message(val author:String,val body:String)
 
 @Composable
 fun MyAppNavHost(
@@ -139,22 +127,20 @@ fun IntroScreen(
     onNavigateToSearch: () -> Unit = {},
     onNavigateToClock: () -> Unit = {}
 ){
-    MaterialTheme{
-        Surface(
-            modifier = Modifier.padding(vertical = 5.dp, horizontal = 20.dp)
+    Surface(
+        modifier = Modifier.padding(vertical = 5.dp, horizontal = 20.dp)
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 5.dp)
         ){
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 5.dp)
-            ){
-                IntroButton(content = "튜토리얼", onclick = onNavigateToTutorial)
-                Spacer(modifier = Modifier.height(15.dp))
-                IntroButton(content = "영화 검색", onclick = onNavigateToSearch)
-                Spacer(modifier = Modifier.height(15.dp))
-                IntroButton(content = "디지털 시계", onclick = onNavigateToClock)
-            }
+            IntroButton(content = "튜토리얼", onclick = onNavigateToTutorial)
+            Spacer(modifier = Modifier.height(15.dp))
+            IntroButton(content = "영화 검색", onclick = onNavigateToSearch)
+            Spacer(modifier = Modifier.height(15.dp))
+            IntroButton(content = "디지털 시계", onclick = onNavigateToClock)
         }
     }
 }
@@ -167,7 +153,7 @@ fun IntroButton(
     Button(
         onClick = onclick,
         colors = ButtonDefaults.buttonColors(Color.DarkGray),
-        shape = RectangleShape,
+        shape = RoundedCornerShape(20),
         modifier = Modifier
             .fillMaxWidth()
             .height(50.dp)
@@ -232,7 +218,8 @@ fun Board(
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+        shape = RoundedCornerShape(10)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
@@ -281,72 +268,96 @@ fun SearchScreen(){
 @Preview
 @Composable
 fun ClockScreen(){
+    var localDateTime:LocalDateTime
+    var localTime by rememberSaveable { mutableStateOf("00 00 00") }
+    
+    LaunchedEffect(localTime) {
+        while(true){
+            localDateTime= LocalDateTime.now()
+            localTime=localDateTime.format(DateTimeFormatter.ofPattern("HH mm ss")).toString()
+            delay(1000)
+        }
+    }
 
+    Row(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Row {
+            Number(localTime[0].toString())
+            Number(localTime[1].toString())
+        }
+        Text(
+            text=":",
+            fontSize = 50.sp,
+            modifier = Modifier.padding(10.dp)
+        )
+        Row {
+            Number(localTime[3].toString())
+            Number(localTime[4].toString())
+        }
+        Text(
+            text=":",
+            fontSize = 50.sp,
+            modifier = Modifier.padding(10.dp)
+        )
+        Row {
+            Number(localTime[6].toString())
+            Number(localTime[7].toString())
+        }
+    }
 }
 
+private val num:List<String> = listOf("1111110","0110000","1101101","1111001","0110011"
+    ,"1011011","1011111","1110000","1111111","1111011")
+@Preview
 @Composable
-fun MessageCard(msg:Message){
-    Row(modifier = Modifier.padding(all=30.dp)){
-        Text(
-            text = "aaa",
-            style = MaterialTheme.typography.bodyMedium
-        )
-
-        Spacer(modifier = Modifier.width(15.dp))
-
-        var isExpanded by remember{ mutableStateOf(false) }
-
-        Column (modifier = Modifier.clickable {isExpanded=!isExpanded}){
-            Text(text = msg.author)
-            Spacer(modifier = Modifier.height(5.dp))
-            Surface (
-                shape = MaterialTheme.shapes.medium,
-                tonalElevation = 5.dp,
-                modifier = Modifier
-                    .animateContentSize()
-                    .padding(1.dp)
-            ){
-                Text(
-                    text = msg.body.repeat(6),
-                    modifier = Modifier.padding(all=4.dp),
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.bodyMedium
-                )
+fun Number(n:String="0"){
+    Surface(
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+    ){
+        Column{
+            Row {
+                Spacer(modifier = Modifier.width(4.dp))
+                NumSegment(filled = num[n.toInt()][0].toString(),0)
+            }
+            Row {
+                NumSegment(filled = num[n.toInt()][5].toString(),1)
+                Spacer(modifier = Modifier.width(30.dp))
+                NumSegment(filled = num[n.toInt()][1].toString(),1)
+            }
+            Row {
+                Spacer(modifier = Modifier.width(4.dp))
+                NumSegment(filled = num[n.toInt()][6].toString(),0)
+            }
+            Row {
+                NumSegment(filled = num[n.toInt()][4].toString(),1)
+                Spacer(modifier = Modifier.width(30.dp))
+                NumSegment(filled = num[n.toInt()][2].toString(),1)
+            }
+            Row {
+                Spacer(modifier = Modifier.width(4.dp))
+                NumSegment(filled = num[n.toInt()][3].toString(),0)
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun PreviewMessageCard(name:String="aa"){
-    MaterialTheme {
-        Surface {
-            MessageCard(msg = Message("Colleague","Hey"))
-        }
-    }
-}
-
-@Composable
-fun Conversation(messages:List<Message>){
-    LazyColumn{
-        items(messages){
-                message -> MessageCard(msg = message)
-        }
-    }
-}
-
-@Composable
-fun PreviewConversation(){
-    MaterialTheme {
-        Conversation(messages = listOf(Message("a", "1"),
-            Message("B", "2"),
-            Message("c", "3"),
-            Message("d", "4"),
-            Message("e", "5"),
-            Message("f", "6"),
-            Message("g", "7"),
-            Message("h", "812312323"),
-            Message("i", "9"),
-            Message("j", "0")))
-    }
+fun NumSegment(filled:String="0",orientation:Int=0){
+    Box(
+        modifier = Modifier
+            .height((orientation * 26 + 4).dp)
+            .width((30 - orientation * 26).dp)
+            .background(
+                when (filled == "1") {
+                    true -> Color.Black
+                    false -> Color.White
+                }
+            )
+    )
 }
