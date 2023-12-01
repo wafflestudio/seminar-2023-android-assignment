53bc03ea478939efa78d3f9718321de49bb986c2
kevin990222 <113081855+kevin990222@users.noreply.github.com>
2023-12-01 18:02:37 +0900

    Tutorial 2

--- assignment5/app/src/main/java/com/wafflestudio/assignment5/MainActivity.kt
+++ assignment5/app/src/main/java/com/wafflestudio/assignment5/MainActivity.kt
@@ -1,21 +1,43 @@
 package com.wafflestudio.assignment5
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.SemanticsProperties.ContentDescription
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -28,49 +50,153 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
 private fun MyApp(
-    modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    modifier: Modifier = Modifier
 ){
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for(name in names){
-            Greeting(name = name)
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if(shouldShowOnboarding){
+            OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+        }else{
+            Greetings()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun OnboardingScreen(
+    modifier: Modifier = Modifier,
+    onContinueClicked: () -> Unit
+){
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Welcome!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ){
+            Text("Continue")
+        }
+    }
+}
 
-    val expanded = remember { mutableStateOf(false) }
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) {"$it"}
+){
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names){
+            name -> Greeting(name = name)
+        }
+    }
+}
 
-    val extraPadding = if(expanded.value) 48.dp else 0.dp
+@Composable
+private fun Greeting(name: String) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ){
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name:String){
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ){
+        Column(modifier = Modifier
+            .weight(1f)
+            .padding(12.dp)
+        ){
+            Text(text = "Hello")
+            Text(text = name,
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            if(expanded){
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
+            }
+        }
+        IconButton(onClick = {expanded = !expanded}) {
+            Icon(
+                imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                ContentDescription = if(expanded){
+                    stringResource(id = R.string.show_less)
+                }else {
+                    stringResource(id = R.string.show_more)
+                }
+            )
+        }
+    }
+
+
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+
+        ), label = ""
+    )
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ){
         Row(modifier = Modifier.padding(24.dp)){
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)
-            ){
-                Text(text = "Hello")
-                Text(text = name)
-            }
+
             ElevatedButton(
-                onClick = { expanded.value = !expanded.value}
+                onClick = { expanded = !expanded}
             ) {
-                Text(if(expanded.value) "Show less" else "Show more")
+                Text(if(expanded) "Show less" else "Show more")
             }
         }
 
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    OnboardingScreen(onContinueClicked = {})
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "dark"
+    )
 @Preview(showBackground = true, widthDp = 320)
 @Composable
-fun DefaultPreview(){
+private fun GreetingPreview(){
+    Greetings()
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun MtAppPreview(){
     MyApp()
 }
