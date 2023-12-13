8b1b9df928e910a6f582e9bc3f71b356d4d19a80
pineldeer <63478081+pineldeer@users.noreply.github.com>
2023-12-14 03:41:44 +0900

    Tutorial 완성

--- assignment5/app/src/main/java/com/example/jet/MainActivity.kt
+++ assignment5/app/src/main/java/com/example/jet/MainActivity.kt
@@ -1,11 +1,219 @@
 package com.example.jet
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.compose.tutorial.SampleData
+import com.example.jet.ui.theme.JetTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.lazy.items
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "MainAc") {
+                composable("MainAc") { MainAc(navController) }
+                composable("Tutorial") { Tutorial(/*...*/) }
+                composable("Movie") { Movie(/*...*/) }
+                composable("Clock") { Clock(/*...*/) }
+                /*...*/
+            }
+            //MainAc(navController)
+        }
+
+
     }
 }
+
+@Composable
+fun MainButton(name:String,route:String,navController: NavController){
+
+
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(80.dp)
+        .padding(15.dp)
+        .clip(RoundedCornerShape(8.dp))
+        .border(3.dp, color = Color.Black)// 모서리 둥글게 설정
+        .background(color = Color.White)
+        .clickable {
+            Log.d("aaaa", "clicked")
+            navController.navigate(route) {
+                //popUpTo("MainAc"){ inclusive = true }
+                Log.d("aaaa", navController.currentBackStackEntry?.destination?.route.toString())
+            }
+        },
+        contentAlignment = Alignment.Center
+
+
+    ){
+        Text(
+            text=name,
+            fontSize = 30.sp,
+        )
+    }
+}
+@Composable
+fun MainAc(navController: NavController){
+    //Text("das")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .border(5.dp, color = Color.Cyan)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        MainButton("Tutorial","Tutorial",navController)
+        MainButton("Movie(LOCKED)","Movie",navController)
+        MainButton("Clock(LOCKED)","Clock",navController)
+    }
+}
+@Composable
+fun Tutorial(){
+    JetTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondaryContainer, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondaryContainer,
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                tonalElevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    JetTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Colleague", "Take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
+}
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    JetTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+@Composable
+fun Movie(){}
+@Composable
+fun Clock(){}
